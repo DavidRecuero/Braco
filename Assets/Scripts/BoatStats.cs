388ec3c53720c9aec188e13f32a0eed1c59b211c
David Recuero <recuero.david@gmail.com>
2018-11-20 21:34:23 +0100

    JAM project

---
+++ Assets/Scripts/BoatStats.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public static class BoatStats
+{
+
+    public static float boatVelocity = 1.0f;
+    public static float boatControl = 1.0f;
+    public static float boatShield = 1.0f;
+    public static float variableTest = 5.0f;
+    public static bool showTutorial = true;
+	public static bool showBTutorial = true;
+/*
+    public static float BoatVelocity
+    {
+        get
+        {
+            return boatVelocity;
+        }
+        set
+        {
+            boatVelocity = value;
+        }
+    }
+
+    public static float BoatControl
+    {
+        get
+        {
+            return boatControl;
+        }
+        set
+        {
+            boatControl = value;
+        }
+    }
+    public static float BoatShield
+    {
+        get
+        {
+            return boatShield;
+        }
+        set
+        {
+            boatShield = value;
+        }
+    }
+    */
+}
