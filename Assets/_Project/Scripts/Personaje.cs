d785e1c51e016d6d1fc4c6b5235443b672d02a90
David Recuero <recuero.david@gmail.com>
2018-11-21 20:16:37 +0100

    Big inventory & Jump
    TO DO:
    Debug Jump
    Test Inventory w Carlos

--- Assets/_Project/Scripts/Personaje.cs
+++ Assets/_Project/Scripts/Personaje.cs
@@ -18,21 +18,42 @@ public class Personaje : MonoBehaviour {
     private float h;
 	private float v;
 
+    [Header("Jump")]    //privatizar cosas despues de debug
+    public bool activeJump;
+    public float jumpForce;
+    public bool isGrounded;        //must be colliding with an object tagged as "ground"
+
+    [Header("Inventory")]   //privatizar cosas despues de debug
+    public bool bigInventoryActive;
+
+    [HideInInspector] public enum Objects { Empty, Velocity, Control, Shield, Mast };
+    private Objects inventory;
+    private bool hammerPicked = false;
+    private bool mastPicked = false;
+
+    public Objects[] bigInventory = new Objects[5] { Objects.Empty, Objects.Empty, Objects.Empty, Objects.Empty, Objects.Empty };
+    public int inventoryCurrentPosition;
+
     [Header("Items GameObjects")]
     public GameObject control;
 	public GameObject velocity;
 	public GameObject shield;
 	public GameObject hammer;
 	public GameObject mast;
-
+    
+    [Header("HUD & UI")]
     public Text message;                  
 	private byte messageAlpha = 0;
 
     public Text tutorial;
 
     public RawImage hammerImage;          
-	public RawImage inventoryImage;         
-	public Texture velaTexture;       
+	public RawImage inventoryImage;
+
+    public RawImage[] bigInventoryHUD;
+    public RawImage[] bigInventorySelectedHUD;
+
+    public Texture velaTexture;       
 	public Texture mastilTexture;        
 	public Texture remacheTexture;        
 	public Texture remoTexture;        
@@ -41,12 +62,7 @@ public class Personaje : MonoBehaviour {
     [HideInInspector] public bool gameEnded;
 
     private Camera fpsCamera;
-
-    enum Objects { Empty, Velocity, Control, Shield, Mast };
-    private Objects inventory;
-    private bool hammerPicked = false;
-    private bool mastPicked = false;
-
+    
     void Start ()
     {
 		gameEnded = false;
@@ -61,6 +77,11 @@ public class Personaje : MonoBehaviour {
 		BoatStats.boatVelocity = 1.0f;
 		BoatStats.boatControl = 1.0f;
 
+        if (bigInventoryActive)
+            inventoryImage.enabled = false;
+        else
+            bigInventoryHUD[0].transform.parent.gameObject.SetActive(false);
+
 		if(!BoatStats.showTutorial)
 			tutorial.gameObject.SetActive(false);               //HUD
 	}
@@ -71,10 +92,32 @@ public class Personaje : MonoBehaviour {
         {
             PlayerMovement();
 
-			if (Input.GetKeyDown ("q"))
-                ThrowAction();
+            if(Input.GetKeyDown(KeyCode.Space) && isGrounded && activeJump)
+            {
+                GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+                isGrounded = false;
+            }
 
-            InventoryManager();
+            if (bigInventoryActive)
+            {
+                SelectInventoryPosition();
+
+                if (Input.GetKeyDown("q"))
+                {
+                    ThrowAction(bigInventory[inventoryCurrentPosition]);
+
+                    inventoryCurrentPosition++;
+                    if (inventoryCurrentPosition == bigInventory.Length)
+                        inventoryCurrentPosition = 0;
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown("q"))
+                    ThrowAction(inventory);
+            }
+
+            InventoryHudManager();
 
             TextAnimation();
         }
@@ -92,7 +135,7 @@ public class Personaje : MonoBehaviour {
 	{
 		if (other.gameObject.CompareTag("Hammer"))
 		{
-			if (Input.GetKeyDown("space") || Input.GetKeyDown("e"))
+			if (Input.GetKeyDown("e"))
 			{
 				hammerPicked = true;
 				other.gameObject.SetActive(false);
@@ -102,67 +145,99 @@ public class Personaje : MonoBehaviour {
 		}
 		else if (other.gameObject.CompareTag("Mast"))
 		{
-			if (Input.GetKeyDown("space") || Input.GetKeyDown("e"))
+			if (Input.GetKeyDown("e"))
 			{
-				if (inventory == Objects.Empty)
-				{
-					other.gameObject.SetActive(false);
-					inventory = Objects.Mast;
-                    ShowText("Mast picked");
+                if (bigInventoryActive)
+                {
+                    PickSomething(Objects.Mast);
+                    other.gameObject.SetActive(false);
                 }
-				else
-                    ShowText("You are carrying something");
+                else
+                {
+                    if (inventory == Objects.Empty)
+                    {
+                        other.gameObject.SetActive(false);
+                        inventory = Objects.Mast;
+                        ShowText("Mast picked");
+                    }
+                    else
+                        ShowText("You are carrying something");
+                }
             }
 		}
 		else if (other.gameObject.CompareTag("Shield"))
 		{
 
-			if (Input.GetKeyDown("space") || Input.GetKeyDown("e"))
-			{ 
-				if(inventory == Objects.Empty)
-				{
-					other.gameObject.SetActive(false);
-					inventory = Objects.Shield;
-                    ShowText("Shield picked");
+			if (Input.GetKeyDown("e"))
+			{
+                if (bigInventoryActive)
+                {
+                    PickSomething(Objects.Shield);
+                    other.gameObject.SetActive(false);
                 }
-				else
-                    ShowText("You are carrying something");
+                else
+                {
+                    if (inventory == Objects.Empty)
+                    {
+                        other.gameObject.SetActive(false);
+                        inventory = Objects.Shield;
+                        ShowText("Shield picked");
+                    }
+                    else
+                        ShowText("You are carrying something");
+                }
             }
 		}
 		else if (other.gameObject.CompareTag("Control"))
 		{
 
-			if (Input.GetKeyDown("space") || Input.GetKeyDown("e"))
+			if (Input.GetKeyDown("e"))
 			{
-				if (inventory == Objects.Empty)
-				{
-					other.gameObject.SetActive(false);
-					inventory = Objects.Control;
-                    ShowText("Control picked");
+                if (bigInventoryActive)
+                {
+                    PickSomething(Objects.Control);
+                    other.gameObject.SetActive(false);
                 }
-				else
-                    ShowText("You are carrying something");
+                else
+                {
+                    if (inventory == Objects.Empty)
+                    {
+                        other.gameObject.SetActive(false);
+                        inventory = Objects.Control;
+                        ShowText("Control picked");
+                    }
+                    else
+                        ShowText("You are carrying something");
+                }
             }
 		}
 		else if (other.gameObject.CompareTag("Velocity"))
 		{
 
-			if (Input.GetKeyDown("space") || Input.GetKeyDown("e"))
+			if (Input.GetKeyDown("e"))
 			{
-				if (inventory == Objects.Empty)
-				{
-					other.gameObject.SetActive(false);
-					inventory = Objects.Velocity;
-                    ShowText("Velocity picked");
-				}
-				else
-                    ShowText("You are carrying something");
+                if (bigInventoryActive)
+                {
+                    PickSomething(Objects.Velocity);
+                    other.gameObject.SetActive(false);
+                }
+                else
+                {
+                    if (inventory == Objects.Empty)
+                    {
+                        other.gameObject.SetActive(false);
+                        inventory = Objects.Velocity;
+                        ShowText("Velocity picked");
+                    }
+                    else
+                        ShowText("You are carrying something");
+                }
 			}
 		}
 		else if (other.gameObject.CompareTag("Boat"))
 		{
 
-			if (Input.GetKeyDown("space") || Input.GetKeyDown("e"))
+			if (Input.GetKeyDown("e"))
 			{
 				if (hammerPicked)
 				{
@@ -226,6 +301,18 @@ public class Personaje : MonoBehaviour {
 		}
 	}  
 
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+            isGrounded = false;
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+
     void ShowText(string text_)
     {
         message.text = text_;
@@ -243,55 +330,123 @@ public class Personaje : MonoBehaviour {
             message.color = new Color32(0, 0, 0, 0);
     }
 
-    void ThrowAction()
+    void ThrowAction(Objects item)
     {
         Vector3 spawnPosition = transform.position + (transform.forward * 2);
         spawnPosition.y += 3f;
 
-        if (inventory == Objects.Shield)
-        {
-            inventory = Objects.Empty;
+        if (item == Objects.Shield)
             Instantiate(shield, spawnPosition, transform.rotation);
-        }
-        else if (inventory == Objects.Control)
-        {
-            inventory = Objects.Empty;
+        else if (item == Objects.Control)
             Instantiate(control, spawnPosition, transform.rotation);
-        }
-        else if (inventory == Objects.Velocity)
-        {
-            inventory = Objects.Empty;
+        else if (item == Objects.Velocity)
             Instantiate(velocity, spawnPosition, transform.rotation);
-        }
-        else if (inventory == Objects.Mast)
-        {
-            inventory = Objects.Empty;
+        else if (item == Objects.Mast)
             Instantiate(mast, spawnPosition, transform.rotation);
+        
+
+        if(bigInventoryActive)
+        {
+            bigInventory[inventoryCurrentPosition] = Objects.Empty;
         }
-        else if (inventory == Objects.Empty)
-            ShowText("Nothing to throw");
+        else
+        {
+            if(item == Objects.Empty)
+                ShowText("Nothing to throw");
+
+            inventory = Objects.Empty;
+        }
     }
 
-    void InventoryManager()
+    void InventoryHudManager()
     {
-        switch (inventory)
+        if (bigInventoryActive)
         {
-            case Objects.Velocity:
-                inventoryImage.texture = velaTexture;
-                break;
-            case Objects.Control:
-                inventoryImage.texture = remoTexture;
-                break;
-            case Objects.Shield:
-                inventoryImage.texture = remacheTexture;
-                break;
-            case Objects.Mast:
-                inventoryImage.texture = mastilTexture;
-                break;
-            default:
-                inventoryImage.texture = emptyTexture;
-                break;
+            for (int i = 0; i < bigInventory.Length; i++)
+            {
+                switch (bigInventory[i])                                        //fills the image with the correspondent object
+                {
+                    case Objects.Velocity:
+                        bigInventoryHUD[i].texture = velaTexture;
+                        break;
+                    case Objects.Control:
+                        bigInventoryHUD[i].texture = remoTexture;
+                        break;
+                    case Objects.Shield:
+                        bigInventoryHUD[i].texture = remacheTexture;
+                        break;
+                    case Objects.Mast:
+                        bigInventoryHUD[i].texture = mastilTexture;
+                        break;
+                    default:
+                        bigInventoryHUD[i].texture = emptyTexture;
+                        break;
+                }
+
+                if (inventoryCurrentPosition == i)
+                    bigInventorySelectedHUD[i].enabled = true;
+                else
+                    bigInventorySelectedHUD[i].enabled = false;
+            }
+
         }
+        else
+        {
+            switch (inventory)
+            {
+                case Objects.Velocity:
+                    inventoryImage.texture = velaTexture;
+                    break;
+                case Objects.Control:
+                    inventoryImage.texture = remoTexture;
+                    break;
+                case Objects.Shield:
+                    inventoryImage.texture = remacheTexture;
+                    break;
+                case Objects.Mast:
+                    inventoryImage.texture = mastilTexture;
+                    break;
+                default:
+                    inventoryImage.texture = emptyTexture;
+                    break;
+            }
+        }
+    }
+
+    void PickSomething(Objects item)
+    {
+        bool itemStacked = false;
+
+        for(int i = 0; i < bigInventory.Length; i++)
+        {
+            if(bigInventory[i] == Objects.Empty && !itemStacked)
+            {
+                bigInventory[i] = item;
+                itemStacked = true;
+            }
+        }
+
+        if(!itemStacked)
+        {
+            ThrowAction(bigInventory[inventoryCurrentPosition]);
+
+            bigInventory[inventoryCurrentPosition] = item;
+        }
+
+    }
+
+    void SelectInventoryPosition()
+    {
+        if      (Input.GetKeyDown("1"))
+            inventoryCurrentPosition = 0;
+        else if (Input.GetKeyDown("2"))
+            inventoryCurrentPosition = 1;
+        else if (Input.GetKeyDown("3"))
+            inventoryCurrentPosition = 2;
+        else if (Input.GetKeyDown("4"))
+            inventoryCurrentPosition = 3;
+        else if (Input.GetKeyDown("5"))
+            inventoryCurrentPosition = 4;
     }
 
     void PlayerMovement()
